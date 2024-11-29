7839b3f89202eb9b28c338e9f7ca386901368643
emanuel-xavier <emanueljx@outlook.com>
2024-11-29 19:53:26 -0300

    feat: add bluetooth gamepad buttons

--- ec2-button-box.ino
+++ ec2-button-box.ino
@@ -1,6 +1,12 @@
+#include <BleConnectionStatus.h>
+#include <BleGamepad.h>
+#include <BleGamepadConfiguration.h>
+
 const int pins[] = {32, 33, 25, 26, 27, 14, 13, 4, 16, 17, 18, 19, 21, 22};
 const int numberOfPins = sizeof(pins) / sizeof(pins[0]);
 int buttonPreviousState[numberOfPins];
+
+BleGamepad bleGamepad;
 
 void setup() {
   Serial.begin(9600);
@@ -9,19 +15,29 @@ void setup() {
     pinMode(pins[i], INPUT_PULLUP); 
     buttonPreviousState[i] = LOW;
   }
+
+  bleGamepad.begin();
+  Serial.println("Setup done");
 }
 
 void loop() {
-  for(int i = 0; i < numberOfPins; i++) {
-    int btnCurrentState = digitalRead(pins[i]);
+  if (bleGamepad.isConnected()) {
+    for(int i = 0; i < numberOfPins; i++) {
+      int btnCurrentState = digitalRead(pins[i]);
 
-    if (btnCurrentState != buttonPreviousState[i]) {
-      if (btnCurrentState == LOW) {
-        Serial.println("button pressed");
-      } else {
-        Serial.println("button released");
+      if (btnCurrentState != buttonPreviousState[i]) {
+        if (btnCurrentState == LOW) {
+          Serial.println("button pressed");
+           bleGamepad.press(BUTTON_1 + i);
+        } else {
+          Serial.println("button released");
+          bleGamepad.release(BUTTON_1 + i);
+        }
+        buttonPreviousState[i] = btnCurrentState;
       }
-      buttonPreviousState[i] = btnCurrentState;
     }
+  } else {
+    Serial.println("bleGamepad is not connected");
   }
+  delay(10);
 }
