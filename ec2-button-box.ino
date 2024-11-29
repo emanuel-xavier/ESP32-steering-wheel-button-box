b6fd27e7731686985ecbbdc8d4c0d8c1bcf8c8b6
emanuel-xavier <emanueljx@outlook.com>
2024-11-29 19:11:46 -0300

    feat: identify when buttons are pressed or released

--- ec2-button-box.ino
+++ ec2-button-box.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+const int pins[] = {32, 33, 25, 26, 27, 14, 13, 4, 16, 17, 18, 19, 21, 22};
+const int numberOfPins = sizeof(pins) / sizeof(pins[0]);
+int buttonPreviousState[numberOfPins];
 
+void setup() {
+  Serial.begin(9600);
+
+  for(int i = 0; i < numberOfPins; i++) {
+    pinMode(pins[i], INPUT_PULLUP); 
+    buttonPreviousState[i] = LOW;
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(int i = 0; i < numberOfPins; i++) {
+    int btnCurrentState = digitalRead(pins[i]);
 
+    if (btnCurrentState != buttonPreviousState[i]) {
+      if (btnCurrentState == LOW) {
+        Serial.println("button pressed");
+      } else {
+        Serial.println("button released");
+      }
+      buttonPreviousState[i] = btnCurrentState;
+    }
+  }
 }
