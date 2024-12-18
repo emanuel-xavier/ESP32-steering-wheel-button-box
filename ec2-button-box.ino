0255f2f49d71f31182d3b9a308a05566c5222763
Emanuel Xavier <emanueljx@outlook.com>
2024-12-17 23:27:54 -0300

    feat: add threads to listen to the inputs

--- ec2-button-box.ino
+++ ec2-button-box.ino
@@ -38,7 +38,6 @@ namespace Encoder {
 }
 
 Bounce debouncers[NUM_OF_BUTTONS];
-Bounce encoderDebouncers[NUM_OF_ENCODERS * 2];
 BleGamepad bleGamepad;
 
 byte buttonPins[NUM_OF_BUTTONS] = {4, 5, 13, 14, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27};
@@ -47,6 +46,51 @@ byte physicalButtons[NUM_OF_BUTTONS + NUM_OF_ENCODERS * 2];
 
 Encoder::Encoder encoders[NUM_OF_ENCODERS];
 byte encoderBtnStart = NUM_OF_BUTTONS;
+
+void buttonTask(void *pvParameters) {
+  while (1) {
+    bool sendReport = false;
+    for (byte i = 0; i < NUM_OF_BUTTONS; i++) {
+      debouncers[i].update();
+      if (debouncers[i].fell()) {
+        bleGamepad.press(physicalButtons[i]);
+        sendReport = true;
+      } else if (debouncers[i].rose()) {
+        bleGamepad.release(physicalButtons[i]);
+        sendReport = true;
+      }
+    }
+    if (sendReport) bleGamepad.sendReport();
+    vTaskDelay(5 / portTICK_PERIOD_MS);
+  }
+}
+
+void encoderTask(void *pvParameters) {
+  while (1) {
+    bool sendReport = false;
+    for (byte i = 0; i < NUM_OF_ENCODERS; i++) {
+      Encoder::EncoderMovement movement = encoders[i].getEncoderMovement();
+      switch (movement) {
+        case Encoder::clockwise:
+          bleGamepad.press(physicalButtons[encoderBtnStart + i * 2]);
+          sendReport = true;
+          bleGamepad.release(physicalButtons[encoderBtnStart + i * 2]);
+          break;
+
+        case Encoder::anticlockwise:
+          bleGamepad.press(physicalButtons[encoderBtnStart + i * 2 + 1]);
+          sendReport = true;
+          bleGamepad.release(physicalButtons[encoderBtnStart + i * 2 + 1]);
+          break;
+
+        default:
+          break;
+      }
+    }
+    if (sendReport) bleGamepad.sendReport();
+    vTaskDelay(5 / portTICK_PERIOD_MS);
+  }
+}
 
 void setupButtons() {
   for (byte i = 0; i < NUM_OF_BUTTONS; i++) {
@@ -60,8 +104,6 @@ void setupEncoders() {
   for (byte i = 0; i < NUM_OF_ENCODERS; i++) {
     encoders[i] = Encoder::Encoder(encoderPins[i][0], encoderPins[i][1]);
     encoders[i].begin();
-    encoderDebouncers[i * 2].interval(DEBOUNCE_DELAY);     // Clockwise button debounce
-    encoderDebouncers[i * 2 + 1].interval(DEBOUNCE_DELAY); // Anti-clockwise button debounce
   }
 }
 
@@ -72,67 +114,20 @@ void setupBleGamepad() {
   bleGamepad.begin(&config);
 }
 
-void processButtons(bool &sendReport) {
-  for (byte i = 0; i < NUM_OF_BUTTONS; i++) {
-    debouncers[i].update();
-    if (debouncers[i].fell()) {
-      bleGamepad.press(physicalButtons[i]);
-      sendReport = true;
-    } else if (debouncers[i].rose()) {
-      bleGamepad.release(physicalButtons[i]);
-      sendReport = true;
-    }
-  }
-}
-
-void processEncoders(bool &sendReport) {
-  for (byte i = 0; i < NUM_OF_ENCODERS; i++) {
-    Encoder::EncoderMovement movement = encoders[i].getEncoderMovement();
-    switch (movement) {
-      case Encoder::clockwise:
-        encoderDebouncers[i * 2].update();
-        if (encoderDebouncers[i * 2].fell()) {
-          bleGamepad.press(physicalButtons[encoderBtnStart + i * 2]);
-          sendReport = true;
-          bleGamepad.release(physicalButtons[encoderBtnStart + i * 2]);
-        }
-        break;
-
-      case Encoder::anticlockwise:
-        encoderDebouncers[i * 2 + 1].update();
-        if (encoderDebouncers[i * 2 + 1].fell()) {
-          bleGamepad.press(physicalButtons[encoderBtnStart + i * 2 + 1]);
-          sendReport = true;
-          bleGamepad.release(physicalButtons[encoderBtnStart + i * 2 + 1]);
-        }
-        break;
-
-      default:
-        break;
-    }
-  }
-}
-
 void setup() {
+  Serial.begin(115200);
   setupButtons();
   setupEncoders();
   setupBleGamepad();
+
   for (byte i = 0; i < (NUM_OF_BUTTONS + NUM_OF_ENCODERS * 2); i++) {
     physicalButtons[i] = i + 1;
   }
-  Serial.begin(115200);
+
+  xTaskCreate(buttonTask, "ButtonTask", 2048, NULL, 1, NULL);
+  xTaskCreate(encoderTask, "EncoderTask", 2048, NULL, 1, NULL);
 }
 
 void loop() {
-  if (bleGamepad.isConnected()) {
-    bool sendReport = false;
-    processButtons(sendReport);
-    processEncoders(sendReport);
 
-    if (sendReport) bleGamepad.sendReport();
-  } else {
-    Serial.println("blegamepad not connected");
-    delay(1000);
-  }
-  delay(10);
 }
